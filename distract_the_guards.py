ec3e6a1451bfd70a79af2c9d8bb58639ce9fd7c6
Karim Abou Nassif <karimabounassif@davids-air.american.edu>
2017-09-01 13:51:20 -0400

    Cleaned code

--- distract_the_guards.py
+++ distract_the_guards.py
@@ -1,48 +1,42 @@
 from fractions import gcd
 
+#Class to keep track of each guard and the pairs they infinitely loop with
 class Guard:
 	def __init__(self, bananas):
 		self.bananaCount = bananas
 		self.infinite = []
-		self.ends = []
 
 
+#Tests if two guards will loop infinitely
 def is_forever(x, y):
     z = (x + y) / gcd(x, y)
     return bool((z - 1) & z)
 
 def answer(Banana_list):
-	banana_list = sorted(Banana_list)
-	length = len(banana_list)
-	guards=[None]*length
-	pairs = 0
-	test = []
-	for x in xrange(length):
-		guards[x]=Guard(banana_list[x])
+    banana_list = sorted(Banana_list)
+    length = len(banana_list)
+    guards=[None]*length
+    pairs = 0
 
-	for x in xrange(length):
-		for y in xrange(length):
-			if is_forever(banana_list[x], banana_list[y]):
-				guards[x].infinite.append(banana_list[y])
-			else:
-				guards[x].ends.append(banana_list[y])
+    for x in xrange(length):
+    	guards[x]=Guard(banana_list[x])
+    
+ 	#Finding the matching pairs of each guard	
+    for x in xrange(length):
+    	for y in xrange(length):
+    		if is_forever(banana_list[x], banana_list[y]):
+    			guards[x].infinite.append(banana_list[y])
 
-	guards.sort(key=lambda x: len(x.infinite))
-	for x in xrange(length):
-		print guards[x].bananaCount, guards[x].infinite
+    #Sorting guards by least amount of pairs. 
+    guards.sort(key=lambda x: len(x.infinite))
 
-	for x in xrange(0, length):
-		for y in xrange(length-1, 1, -1):
-			if guards[x].bananaCount in guards[y].infinite and guards[y].bananaCount in guards[x].infinite:
-				print guards[x].bananaCount, guards[y].bananaCount
-				pairs +=1 
-				guards[x].infinite = []
-				guards[y].infinite = []
-				test.append(guards[x].bananaCount)
-				test.append(guards[y].bananaCount)
-	return length-(pairs*2)
-				
-	print test
-	print pairs
-
-print answer([1, 7, 3, 21, 13, 19])
+    #While not perfect, by matching guards starting with those who have
+    #the least amount of possible pairs, a good approximation is reached
+    #as to the combinations that result in the most paired guards.
+    for x in xrange(0, length):
+    	for y in xrange(length-1, 1, -1):
+    		if guards[x].bananaCount in guards[y].infinite and guards[y].bananaCount in guards[x].infinite:
+    			pairs +=1 
+    			guards[x].infinite = []
+    			guards[y].infinite = []
+    return length-(pairs*2)
